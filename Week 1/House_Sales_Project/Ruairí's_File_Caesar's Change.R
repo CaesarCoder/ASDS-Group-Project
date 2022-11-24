e286979342219315b365119248430384a1ed315a
CaesarCoder <zhangt4@tcd.ie>
2022-11-24 16:58:30 +0000

    Squared/pre-squared Living Standard

--- Week 1/House_Sales_Project/Ruairí's_File_Caesar's Change.R
+++ Week 1/House_Sales_Project/Ruairí's_File_Caesar's Change.R
@@ -7,10 +7,13 @@ dat <- read.table ("https://raw.githubusercontent.com/gedeck/practical-statistic
 
 install.packages("dplR")
 install.packages("stargazer")
+install.packages("callr")
+library(callr)
 library(stargazer)
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
+library(car)
 
 # Landvalue and bathrooms multivariate regression
 lm(dat$AdjSalePrice ~ dat$Bathrooms + dat$LandVal)
@@ -133,7 +136,152 @@ summary(lm.4)
 stargazer(lm.4, type = "html")
 # we can say that lm.4 is the best model of prediction. 
 
+scatter.smooth(dat$SqFtTotLiving, resid(lm.4), # plot a smooth line on the scatter plot
+               lpars = list(col = "blue", lwd = 3, lty = 3), 
+               main = "Residual Plot (Sale Price ~ Size)",
+               xlab = "Total Living Area (sq.ft.)",
+               ylab = "Residuals")
+abline(h = 0, col = "red")
 
+par(mfrow = c(1,1))
+avPlot(lm.4, variable = "SqFtTotLiving")
+
+avPlot(lm.4, variable = "BldgGrade")
+
+
+scatter.smooth(dat$Bedrooms, resid(lm.4), # plot a smooth line on the scatter plot
+               lpars = list(col = "blue", lwd = 3, lty = 3), 
+               main = "Residual Plot (Sale Price ~ Size)",
+               xlab = "Total Living Area (sq.ft.)",
+               ylab = "Residuals")
+abline(h = 0, col = "red")
+
+terms <- predict(lm.4, type = "terms") # extract the individual regression terms from our model for each observation
+
+partial_resid <- resid(lm.4) + terms # add the individual regression terms to the residual for each observation
+
+df <- data.frame(SqFtTotLiving = dat[, "SqFtTotLiving"], # create a new data.frame of these vals
+                 Terms = terms[, "SqFtTotLiving"],
+                 PartialResid = partial_resid[, "SqFtTotLiving"])
+
+ggplot(df, aes(SqFtTotLiving, PartialResid)) +
+  geom_point(alpha = 0.2) +
+  geom_smooth() +
+  geom_line(aes(SqFtTotLiving, Terms), colour = "red")
+
+lm.5 <- lm(AdjSalePrice ~ Bathrooms + I(SqFtTotLiving^2) + SqFtTotLiving + BldgGrade + ZipGroup, data = dd)
+summary(lm.5)
+
+terms_poly <- predict(lm.5, type = "terms") # extract the individual regression terms from our model for each observation
+
+partial_resid_poly <- resid(lm.5) + terms_poly # add the individual regression terms to the residual for each observation
+
+df_poly <- data.frame(SqFtTotLiving = dat[, "SqFtTotLiving"], # create a new data.frame of these vals
+                      Terms = terms_poly[, "I(SqFtTotLiving^2)"],
+                      PartialResid = partial_resid_poly[, "I(SqFtTotLiving^2)"])
+
+ggplot(df_poly, aes(SqFtTotLiving, PartialResid)) +
+  geom_point(alpha = 0.2) +
+  geom_smooth() +
+  geom_line(aes(SqFtTotLiving, Terms), colour = "red")
+
+par(mfrow = c(2, 2)) # we change the graphic device to show 4 plots at once
+plot(mod4) # we supply our lm object to plot()
+
+## final model 
+mod7 <- lm(AdjSalePrice ~ Bathrooms + I(SqFtTotLiving^2) +
+             SqFtTotLiving + BldgGrade + ZipGroup, + BldgGrade,
+           I(BldgGrade^2), data = dat2)
+
+
+
+
+
+
+
+########### 
+
+mod4 <- lm(AdjSalePrice ~ SqFtTotLiving + I(SqFtTotLiving^2) + SqFtLot + Bathrooms + Bedrooms + BldgGrade + PropertyType + as.factor(ZipGroup), data = dd)
+
+terms_poly <- predict(mod4, type = "terms") # extract the individual regression terms from our model for each observation
+
+partial_resid_poly <- resid(mod4) + terms_poly # add the individual regression terms to the residual for each observation
+
+df_poly <- data.frame(SqFtTotLiving = dat[, "SqFtTotLiving"], # create a new data.frame of these vals
+                      Terms = terms_poly[, "I(SqFtTotLiving^2)"],
+                      PartialResid = partial_resid_poly[, "I(SqFtTotLiving^2)"])
+
+ggplot(df_poly, aes(SqFtTotLiving, PartialResid)) +
+  geom_point(alpha = 0.2) +
+  geom_smooth() +
+  geom_line(aes(SqFtTotLiving, Terms), colour = "red")
+
+
+
+
+mod5 <- lm(AdjSalePrice ~ SqFtTotLiving + I(SqFtTotLiving^2) + SqFtLot + Bathrooms + (Bedrooms)^2 + BldgGrade + PropertyType + as.factor(ZipGroup), data = dd)
+
+terms_poly <- predict(mod5, type = "terms") # extract the individual regression terms from our model for each observation
+
+partial_resid_poly <- resid(mod5) + terms_poly # add the individual regression terms to the residual for each observation
+
+df_poly <- data.frame(SqFtTotLiving = dat[, "SqFtTotLiving"], # create a new data.frame of these vals
+                      Terms = terms_poly[, "I(SqFtTotLiving^2)"],
+                      PartialResid = partial_resid_poly[, "I(SqFtTotLiving^2)"])
+
+ggplot(df_poly, aes(SqFtTotLiving, PartialResid)) +
+  geom_point(alpha = 0.2) +
+  geom_smooth() +
+  geom_line(aes(SqFtTotLiving, Terms), colour = "red")
+
+###########
+
+terms <- predict(mod2, type = "terms") # extract the individual regression terms from our model for each observation
+
+partial_resid <- resid(mod2) + terms # add the individual regression terms to the residual for each observation
+
+df <- data.frame(SqFtTotLiving = dat[, "SqFtTotLiving"], # create a new data.frame of these vals
+                 Terms = terms[, "SqFtTotLiving"],
+                 PartialResid = partial_resid[, "SqFtTotLiving"])
+
+ggplot(df, aes(SqFtTotLiving, PartialResid)) +
+  geom_point(alpha = 0.2) +
+  geom_smooth() +
+  geom_line(aes(SqFtTotLiving, Terms), colour = "red")
+
+
+
+
+mod4 <- lm(AdjSalePrice ~ SqFtTotLiving + I(SqFtTotLiving^2) + SqFtLot + Bathrooms + Bedrooms + BldgGrade + PropertyType + as.factor(ZipGroup), data = dd)
+
+
+
+stargazer(mod1, mod2, mod4, type = "html")
+
+
+
+
+terms_poly <- predict(mod4, type = "terms") # extract the individual regression terms from our model for each observation
+
+partial_resid_poly <- resid(mod4) + terms_poly # add the individual regression terms to the residual for each observation
+
+df_poly <- data.frame(SqFtTotLiving = dat[, "SqFtTotLiving"], # create a new data.frame of these vals
+                      Terms = terms_poly[, "I(SqFtTotLiving^2)"],
+                      PartialResid = partial_resid_poly[, "I(SqFtTotLiving^2)"])
+
+ggplot(df_poly, aes(SqFtTotLiving, PartialResid)) +
+  geom_point(alpha = 0.2) +
+  geom_smooth() +
+  geom_line(aes(SqFtTotLiving, Terms), colour = "red")
+
+
+par(mfrow = c(2, 2)) # we change the graphic device to show 4 plots at once
+plot(mod4) # we supply our lm object to plot()
+
+
+sresid <- rstandard(mod4) # the rstandard() function extracts standardised residuals
+index <- order(sresid) # make an index of standardised residuals
+dat[index[1:5], c("AdjSalePrice", "SqFtTotLiving", "SqFtLot", "Bathrooms", "Bedrooms", "BldgGrade")]
 
 
 
